49a945f8eb7247d3042bffd59f6c9c5a5a81ada5
이승렬 <zxqw1541@naver.com>
2015-12-24 12:40:17 +0900

    ????

---
+++ 0project03/src/main/java/java76/pms/service/BoardService.java
@@ -0,0 +1,21 @@
+package java76.pms.service;
+
+import java.util.List;
+
+import java76.pms.domain.Board;
+
+public interface BoardService {
+  List<Board> getBoardList(int pageNo,
+      int pageSize, String keyword, String align);
+  void register(Board board);
+  void remove(int no, String password);
+  void change(Board board);
+  Board retrieve(int no);
+}
+
+
+
+
+
+
+
---
+++ 0project03/src/main/java/java76/pms/service/CourseEnrollmentService.java
@@ -0,0 +1,15 @@
+package java76.pms.service;
+
+import java.util.List;
+
+import java76.pms.domain.CourseEnrollment;
+
+public interface CourseEnrollmentService {
+  void enroll(CourseEnrollment enroll);
+  void change(CourseEnrollment enroll);
+  void remove(String email);
+  CourseEnrollment retrieveByEmail(String email);
+  List<CourseEnrollment> getEntrollmentList();
+  void reject(String email);
+  void approve(String email);
+}
